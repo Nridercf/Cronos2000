8c250f6080bc2804197e42707975e852790b3cc3
YaroslavLeGrand <132467006+YaroslavLeGrand@users.noreply.github.com>
2024-03-23 16:01:06 +0100

    Create Utilisateur.cs

---
+++ DFRCronos2000/Models/Utilisateur.cs
@@ -0,0 +1,12 @@
+namespace DFRCronos2000.Models;
+
+public class Utilisateur
+{
+    public int? IdUtil { get; set; }
+    public string Nom { get; set; }
+    public string Prenom { get; set; }
+    public string Matricule { get; set; }
+    public string Mdp { get; set; }
+
+    public int IdRole { get; set; }
+}
